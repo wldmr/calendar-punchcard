2c22067e03fe33f0deecea95f31cf84f5c61bc65
Andreas Waldenburger <andreas@waldenburger.name>
2015-04-04 14:47:20 +0200

    Changed main activity to a stupid, static list.

--- src/net/wldmr/CalendarTimesheet/CalendarTimesheetActivity.java
+++ src/net/wldmr/CalendarTimesheet/CalendarTimesheetActivity.java
@@ -3,6 +3,9 @@ package net.wldmr.CalendarTimesheet;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
 public class CalendarTimesheetActivity extends Activity
 {
     /** Called when the activity is first created. */
@@ -11,5 +14,13 @@ public class CalendarTimesheetActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        String[] dummyItems = {"One, potato", "Two, potato"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        R.layout.calendar_list_item, dummyItems);
+
+        ListView listView = (ListView) findViewById(R.id.calendar_list);
+        listView.setAdapter(adapter);
     }
 }
