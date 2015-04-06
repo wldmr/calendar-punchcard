2c39397768f9cb2f23210a88c912737b21e13f93
Andreas Waldenburger <andreas@waldenburger.name>
2015-04-06 23:29:28 +0200

    Made custom View Adapter to display calendar color.
    :) ... :( ... :) ... :/
    
    That was annoying.

--- src/net/wldmr/CalendarTimesheet/CalendarTimesheetActivity.java
+++ src/net/wldmr/CalendarTimesheet/CalendarTimesheetActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 
 import android.provider.CalendarContract.Calendars;
@@ -11,18 +12,46 @@ import android.provider.CalendarContract.Calendars;
 import android.database.Cursor;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.Context;
 import android.net.Uri;
 
 import android.widget.AdapterView;
 import android.view.View;
+import android.view.ViewGroup;
 
 import android.widget.Toast;
 
 import android.util.Log;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
 public class CalendarTimesheetActivity extends Activity
 {
     private Cursor cur;
+
+    private class MyCursorAdapter extends SimpleCursorAdapter {
+
+        MyCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
+            super(context, layout, c, from, to, flags);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // FIXME: Sooooper inefficient, probably. There must be a better 
+            // way to set the color on that view, and even if not, we shouldn't 
+            // regenerate the bitmap every time the view is rendered. Oh well.
+            View view = super.getView(position, convertView, parent);
+            ImageView img = (ImageView) view.findViewById(R.id.calendar_color);
+            Bitmap bitmap = Bitmap.createBitmap(1,1, Bitmap.Config.ARGB_8888);
+            Cursor c = getCursor();
+            c.moveToPosition(position);
+            String color = c.getString(5); // FIXME: A more robust way of getting the column
+            bitmap.eraseColor(Integer.parseInt(color));
+            img.setImageBitmap(bitmap);
+            return view;
+        }
+    }
 
     /** Called when the activity is first created. */
     @Override
@@ -37,12 +66,14 @@ public class CalendarTimesheetActivity extends Activity
 
         String[] fromColumns = {
             Calendars.CALENDAR_DISPLAY_NAME,
+            Calendars.CALENDAR_COLOR,
         };
         int[] toViews = {
             R.id.display_name,
+            R.id.calendar_color,
         };
 
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+        SimpleCursorAdapter adapter = new MyCursorAdapter(this,
                 R.layout.calendar_list_item, cur, fromColumns, toViews, 0);
         ListView listView = (ListView) findViewById(R.id.calendar_list);
         listView.setAdapter(adapter);
